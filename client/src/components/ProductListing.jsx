5ca13488dbd906a18530c8aa0feaa84964d26460
dmarinov1 <dmarinov@westum.com>
2019-07-02 00:11:26 +0300

    Dropdown for category

--- client/src/components/ProductListing.jsx
+++ client/src/components/ProductListing.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button, Card, Container, Row } from 'react-bootstrap'
+import { Button, Card, Container, Row, Dropdown } from 'react-bootstrap'
 import { productsSelector } from '../reducers/products'
 import { fetchProductsAction } from './../actions/products'
 
@@ -18,36 +18,53 @@ export class ProductListing extends Component {
         }))
 
         return (
-            <Container style={{ padding: '5px' }}>
-                <Row>
-                    {/* <Col sm={8}> */}
+            <div>
+                <div>
+                    <Dropdown style={{ padding: '5px' }}>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Categories
+                        </Dropdown.Toggle>
 
-                    {normalizedProducts.map(product => (
-                        <Card style={{ width: '18rem' }} key={product._id}>
-                            <Card.Img
-                                style={{
-                                    position: 'relative',
-                                    width: '288px',
-                                    height: '180px',
-                                    padding: '5px',
-                                    cursor: 'pointer',
-                                }}
-                                variant="top"
-                                src={`${product.photo}`}
-                                onClick={() => this.props.history.push(`/product-details/${product._id}`)}
-                            />
-                            <Card.Body>
-                                <Card.Title>{product.title}</Card.Title>
-                                <Card.Text>{product.shortDescription}</Card.Text>
-                                <Button variant="primary" block>
-                                    ADD TO CART
-                                </Button>
-                            </Card.Body>
-                        </Card>
-                    ))}
-                    {/* </Col> */}
-                </Row>
-            </Container>
+                        <Dropdown.Menu>
+                            {['Dildota', 'Vibratori', 'Pedali'].map(category => (
+                                <Dropdown.Item href="#/action-1">{category}</Dropdown.Item>
+                            ))}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+                <div>
+                    <Container style={{ padding: '5px' }}>
+                        <Row>
+                            {/* <Col sm={8}> */}
+
+                            {normalizedProducts.map(product => (
+                                <Card style={{ width: '18rem' }} key={product._id}>
+                                    <Card.Img
+                                        style={{
+                                            position: 'relative',
+                                            width: '288px',
+                                            height: '180px',
+                                            padding: '5px',
+                                            cursor: 'pointer',
+                                        }}
+                                        variant="top"
+                                        src={`${product.photo}`}
+                                        onClick={() => this.props.history.push(`/product-details/${product._id}`)}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>{product.title}</Card.Title>
+                                        <Card.Text>{product.shortDescription}</Card.Text>
+                                        <Button variant="primary" block>
+                                            ADD TO CART
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            ))}
+                            {/* </Col> */}
+                        </Row>
+                    </Container>
+                </div>
+            </div>
         )
     }
 }
